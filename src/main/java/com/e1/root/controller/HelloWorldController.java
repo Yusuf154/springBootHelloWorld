40a9916e8b82288263c2df3a12046c48aaf02c82
Yusuf154 <yusufjava8@gmail.com>
2022-06-26 09:04:44 -0700

    initial commit

---
+++ src/main/java/com/e1/root/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.e1.root.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/hello")
+	public String show() {
+		
+		return "hello world this is first project";
+	} 
+
+}
---
+++ src/test/java/com/e1/root/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e1.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
