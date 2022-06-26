e02662b42ce72c4ebe666c745307c1ade90d0c6b
Yusuf154 <yusufjava8@gmail.com>
2022-06-26 23:11:39 +0530

    second commit

--- src/main/java/com/e1/root/SpringBootHelloWorldApplication.java
+++ src/main/java/com/e1/root/SpringBootHelloWorldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootHelloWorldApplication {
+public class SpringBootHelloWorldApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
