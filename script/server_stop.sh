8f6fd832c3f9efa733edbc6a4fa8278207693c74
Yusuf154 <yusufjava8@gmail.com>
2023-04-26 16:39:21 +0530

    updated

--- script/server_stop.sh
+++ script/server_stop.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-sudo killall java
-exit 0
+# Kill Java process
+
+# Determine the pid
+echo "killing process starts"
+PID=`ps -C java -o pid=`
+
+if [ -n "$PID" ]; then
+    echo "killing process"
+    kill -9 $PID
+else
+    echo "empty"
+fi
+
+cd /opt
+rm -rf root
