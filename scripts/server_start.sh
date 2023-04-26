80321dab7ad16528358680cd1fc21db3d9cc7454
Yusuf154 <yusufjava8@gmail.com>
2023-04-26 19:08:20 +0530

    updated for deployment

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=8282 \
-    *.jar > /test/null 2> /test/null < /test/null &
+sudo /usr/bin/java -jar -Dserver.port=8181 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
--- server_start.sh
+++ server_start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-sudo mkdir server
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=80 \
+sudo /usr/bin/java -jar -Dserver.port=8181 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
