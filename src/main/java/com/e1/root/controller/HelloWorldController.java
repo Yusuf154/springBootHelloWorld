3baec741786b1d2a0d2f3af485cbe167f5c695e9
Yusuf154 <yusufjava8@gmail.com>
2023-04-28 00:02:25 +0530

    updated

--- src/main/java/com/e1/root/controller/HelloWorldController.java
+++ src/main/java/com/e1/root/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 	@GetMapping("/hello")
 	public String show() {
 		
-		return "hello world this is first projectttttttttttttuuuuuv7";
+		return "hello world this is first projectttttttttttttuuuuuv8";
 	} 
 
 }
