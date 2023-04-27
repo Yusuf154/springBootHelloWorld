53c01c071e970c3aee5844099e70175d175aff5a
Yusuf154 <yusufjava8@gmail.com>
2023-04-28 00:16:36 +0530

    activated two different ports

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,3 +2,6 @@
 cd /home/ec2-user/server
 sudo /usr/bin/java -jar -Dserver.port=8181 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
+    
+sudo /usr/bin/java -jar -Dserver.port=8282 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
