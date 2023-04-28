f692c377cd3a9bba3bb9059c4d949bcae3fca299
Yusuf154 <yusufjava8@gmail.com>
2023-04-28 22:34:33 +0530

    updated

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=8181 \
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "helloWorldDeploymentGroup" ]
+then
+	cd /home/ec2-user/server
+	sudo /usr/bin/java -jar -Dspring.profiles.active=dev -Dserver.port=8181 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
+fi
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "helloWorldTestDeployGroup" ]
+then
+	cd /home/ec2-user/server
+	sudo /usr/bin/java -jar -Dspring.profiles.active=test -Dserver.port=8282 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
+fi
--- src/main/java/com/e1/root/controller/HelloWorldController.java
+++ src/main/java/com/e1/root/controller/HelloWorldController.java
@@ -8,8 +8,8 @@ public class HelloWorldController {
 	
 	@GetMapping("/hello")
 	public String show() {
-		System.out.println("hello world this is first projectttttttttttttuuuuuv18");
-		return "hello world this is first projectttttttttttttuuuuuv18";
+		System.out.println("hello world this is first projectttttttttttttuuuuuv19");
+		return "hello world this is first projectttttttttttttuuuuuv19";
 	} 
 
 }
