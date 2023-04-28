c63428285693efe0c4e4dcbb5e753300caafc064
Yusuf154 <yusufjava8@gmail.com>
2023-04-28 14:48:26 +0530

    updated

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=8282 \
-    *.jar > /test/null 2> /test/null < /test/null &
-    
+sudo /usr/bin/java -jar -Dserver.port=8383 \
+    *.jar > /stage/null 2> /stage/null < /stage/null &
